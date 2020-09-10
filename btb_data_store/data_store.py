573b0c22d93203e899285a1ac2fbaeac6e866ea5
squidboy3999 <58859289+squidboy3999@users.noreply.github.com>
2020-09-10 13:27:44 -0700

    Create data_store.py
    initial data store class

---
+++ btb_data_store/data_store.py
@@ -0,0 +1,22 @@
+class DataStore:
+    
+    def __init__(self):
+        pass
+        
+    def get_unit(self,key):
+        pass
+       
+    def save_unit(self,key,unit_dict):
+        pass
+        
+    def get_world(self,key):
+        pass
+        
+    def save_world(self,key,world_dict):
+        pass
+        
+    def get_territory(self,key):
+        pass
+        
+    def save_territory(self,key,territory_dict):
+        pass
